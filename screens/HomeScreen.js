9c67c52a979f63c56d0154e1df59dfe79c757a35
omkarnazarkar <omkarnazarkar@gmail.com>
2021-04-13 00:59:33 +0530

    Added react-navigation with stack and drawer navigator. Also using react native vector icons for app

---
+++ screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button 
+        title="Go to details screen"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+});
